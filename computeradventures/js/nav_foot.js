3d908011704869b5767ca7eaca73758167d25e7b
Ali Tai <alictai@gmail.com>
2016-01-13 22:35:30 -0800

    loading footer

--- computeradventures/js/nav_foot.js
+++ computeradventures/js/nav_foot.js
@@ -1,18 +1,4 @@
 $(document).ready(function(){
-	$("#nav").load("nav.html");
- //    $.ajax({
- //    	url: 'nav.txt', 
- //    	type: 'GET',
- //    	dataType: 'TEXT',
- //    	success: function(result){
- //     	    $("#nav").load(result);
- //     	    console.log(result);
- //     	    $("#nav").append("<p>hello</p>");
- //        	console.log("success");
- //        	//console.log(result);
- //    	},
- //    	error: function (req, text, error) {
- //    		alert(error);
- //    	}
-	// });
+	$("#nav").load("includes/nav.html");
+	$("footer").load("includes/footer.html");
 });
