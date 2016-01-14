9de6b0377b1db32a41e868099073dfb9679c877d
Ali Tai <alictai@gmail.com>
2016-01-13 21:44:23 -0800

    everything is broken pls help

--- computeradventures/js/nav_foot.js
+++ computeradventures/js/nav_foot.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     	type: 'GET',
     	dataType: 'TEXT',
     	success: function(result){
-     	    $("#nav").append(result);
+     	    $("#nav").html(result);
         	console.log("success");
         	//console.log(result);
     	},
