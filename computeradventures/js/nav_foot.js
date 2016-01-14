5b2491ecfb155215910273e4857cb4f041ce6e3f
Ali Tai <alictai@gmail.com>
2016-01-13 22:30:14 -0800

    trying laod

--- computeradventures/js/nav_foot.js
+++ computeradventures/js/nav_foot.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-    $.ajax({
-    	url: 'nav.txt', 
-    	type: 'GET',
-    	dataType: 'TEXT',
-    	success: function(result){
-     	    $("#nav").innerHTML()=result;
-     	    console.log(result);
-     	    $("#nav").append("<p>hello</p>");
-        	console.log("success");
-        	//console.log(result);
-    	},
-    	error: function (req, text, error) {
-    		alert(error);
-    	}
-	});
+	$("#nav").load("nav.html");
+ //    $.ajax({
+ //    	url: 'nav.txt', 
+ //    	type: 'GET',
+ //    	dataType: 'TEXT',
+ //    	success: function(result){
+ //     	    $("#nav").load(result);
+ //     	    console.log(result);
+ //     	    $("#nav").append("<p>hello</p>");
+ //        	console.log("success");
+ //        	//console.log(result);
+ //    	},
+ //    	error: function (req, text, error) {
+ //    		alert(error);
+ //    	}
+	// });
 });
