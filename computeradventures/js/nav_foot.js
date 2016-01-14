3201fd3fbbb09e2c50597f4e3cd683ff1d0996b4
Ali Tai <alictai@gmail.com>
2016-01-13 23:37:03 -0800

    all the error msgs

--- computeradventures/js/nav_foot.js
+++ computeradventures/js/nav_foot.js
@@ -3,9 +3,11 @@ $(document).ready(function(){
 		if(status == "success") {
 			console.log("navigation loaded");
 			$(".nav-link").removeClass("active");
+			console.log("attempted to remove active class");
 			curr_page = "#nav-" + $(".nav").attr('id');
 			console.log("current page is "+curr_page);
 			$(curr_page).addClass("active");
+			console.log("attempted to add active class");
 		} else {
 			console.log("navigation failed");
 		}
