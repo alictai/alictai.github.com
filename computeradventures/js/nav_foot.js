23bb0f607290cb129f409ca464aa3ed752abd81d
Ali Tai <alictai@gmail.com>
2016-01-13 23:15:36 -0800

    testing

--- computeradventures/js/nav_foot.js
+++ computeradventures/js/nav_foot.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
-	$("#nav").load("includes/nav.html");
-	$("footer").load("includes/footer.html");
+	$(".nav").load("includes/nav.html", function(response, status, xhr) {
+		if(status == "success") {
+			console.log("navigation loaded");
+		} else {
+			console.log("navigation failed");
+		}
+	});
+	$(".nav-link").removeClass("active")
+	$($(".nav").attr('id')).addClass("active");
+	$("footer").load("includes/footer.html", function(response, status, xhr) {
+		if(status == "success") {
+			console.log("footer loaded");
+		} else {
+			console.log("footer failed");
+		}
+	});
 });
