8f4283b877579aca93e358ffacc239c7e5a04fc9
Ali Tai <alictai@gmail.com>
2016-01-13 23:25:43 -0800

    removed active default

--- computeradventures/js/nav_foot.js
+++ computeradventures/js/nav_foot.js
@@ -2,14 +2,13 @@ $(document).ready(function(){
 	$(".nav").load("includes/nav.html", function(response, status, xhr) {
 		if(status == "success") {
 			console.log("navigation loaded");
+			curr_page = "#" + $(".nav").attr('id')
+			console.log("current page is "+curr_page);
+			$(curr_page).addClass("active");
 		} else {
 			console.log("navigation failed");
 		}
 	});
-	$(".nav-link").removeClass("active")
-	curr_page = "#" + $(".nav").attr('id')
-	console.log("current page is "+curr_page);
-	$(curr_page).addClass("active");
 	$("footer").load("includes/footer.html", function(response, status, xhr) {
 		if(status == "success") {
 			console.log("footer loaded");
