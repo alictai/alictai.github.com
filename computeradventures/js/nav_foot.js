26d8949c8ef5bff7aefcad3da866f75667b10993
Ali Tai <alictai@gmail.com>
2016-01-14 00:00:01 -0800

    removed console logs for success

--- computeradventures/js/nav_foot.js
+++ computeradventures/js/nav_foot.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
 	$(".nav").load("includes/nav.html", function(response, status, xhr) {
 		if(status == "success") {
-			console.log("navigation loaded");
+			// console.log("navigation loaded");
 			$(".nav-link").removeClass("active");
-			console.log("attempted to remove active class");
+			// console.log("attempted to remove active class");
 			curr_page = "#nav-" + $(".nav").attr('id');
-			console.log("current page is "+curr_page);
+			// console.log("current page is "+curr_page);
 			$(curr_page).addClass("active");
-			console.log("attempted to add active class");
+			// console.log("attempted to add active class");
 		} else {
 			console.log("navigation failed");
 		}
 	});
 	$("footer").load("includes/footer.html", function(response, status, xhr) {
 		if(status == "success") {
-			console.log("footer loaded");
+			// console.log("footer loaded");
 		} else {
 			console.log("footer failed");
 		}
