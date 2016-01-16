6ac02c132459cd3dfb4597b91263f6f2694fcf59
Ali Tai <alictai@gmail.com>
2016-01-15 22:58:34 -0800

    updated course functionality

--- computeradventures/js/courses.js
+++ computeradventures/js/courses.js
@@ -1,7 +1,6 @@
 var active_filter = "All";
 var btnlookup =	{"All": "All",
-				"3D-Model":"3D Modeling",
-				"3D-Print":"3D Printing",
+				"3D-Creations":"3D Creations",
 				"Animation":"Animation",
 				"App-Dev":"App Development",
 				"Digi-Graphics":"Digital Graphics",
@@ -27,35 +26,6 @@ $('.course-filter').click(function(){
 		$(".course-filter").removeClass("active");
 		$(this).addClass("active");
 	}
-
-	// if (!($(this).hasClass("active"))){
-	// 	if ($("#All").hasClass("active")){
-	// 		$("#All").removeClass("active");
-	// 	}
-
-	// 	active_filters.push(btnlookup[category])
-	// 	$(this).addClass("active");
-	// 	// console.log(active_filters);
-
-	// 	if (active_filters.length == max_categories) {
-	// 		$("#All").addClass("active");
-	// 	}
-	// } else {
-	// 	if (category != "All") {
-	// 		$("#All").removeClass("active");
-	// 		var index = active_filters.indexOf(btnlookup[category])
-	// 		active_filters.splice(index,1);
-	// 		$(this).removeClass("active");
-	// 		// console.log(active_filters);
-	// 	}
-
-	// 	if (active_filters.length == 0) {
-	// 		$("#All").addClass("active");
-	// 	}
-	// 	else if (active_filters.length < max_categories) {
-	// 		$("#All").removeClass("active");
-	// 	}
-	// }
 	$(".course-listing").remove();
 	renderClasses();
 });
@@ -66,13 +36,13 @@ function renderClasses() {
 	for (var i = 0; i < catalog.length; i++) {
 		if (active_filter == "All" || active_filter == catalog[i].Course_Categories) {
 			num_courses += 1;
-			var row_num = Math.floor(num_courses/4);
+			var row_num = Math.floor(num_courses/3);
 			var coursestring = "";
 			
-			coursestring += "<div class=\"col-sm-6 col-md-3 course-listing row-number-"+row_num+"\">";
+			coursestring += "<div class=\"col-md-4 course-listing row-number-"+row_num+"\">";
 			coursestring += "<div class=\"thumbnail featured-product\">";
-			if (catalog[i].Img){
-				coursestring += "<img src=\"./images/"+catalog[i].Img+"\" alt=\"\">";
+			if (catalog[i].Image){
+				coursestring += "<img src=\"./images/"+catalog[i].Image+"\" alt=\"\">";
 			} else {
 				coursestring += "<img src=\"./images/Logo-2.jpg\" alt=\"\">";
 			}
@@ -88,7 +58,7 @@ function renderClasses() {
 	        coursestring += "<h5>"+catalog[i].Age+" | "+catalog[i].Course_Length+"</h5>";
 	        coursestring += "<p>Prerequisite: "+catalog[i].Prerequisite+"</p>";
 	        coursestring += "<p>"+catalog[i].Course_Description+"</p>";
-	        coursestring += "<a class=\"btn btn-primary\" href=\"http://register.asapconnected.com/default.aspx?org=2346\" target=\"_blank\">Register Now!</a>";
+	        coursestring += "<a class=\"btn btn-primary form-control\" href=\"http://register.asapconnected.com/default.aspx?org=2346\" target=\"_blank\">Register Now!</a>";
 			coursestring += "</div></div></div>";
 
 			$("#catalog").append(coursestring);
@@ -96,7 +66,7 @@ function renderClasses() {
 		}
 	}
 
-	var num_rows = Math.ceil(num_courses / 4);
+	var num_rows = Math.ceil(num_courses / 3);
 	for (var j = 0; j < num_rows; j++) {
 		// console.log("wrapping row " + j);
 		var forWrapping = ".row-number-"+j;
